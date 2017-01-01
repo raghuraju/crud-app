870a6a9868fc1995a90e63b3de558f5afcac1e2a
Raghu K <rgh.199@gmail.com>
2017-01-01 23:37:11 +0530

    Step 3: Wrap application with Provider and pass store to it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root.js';
@@ -15,6 +16,8 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
