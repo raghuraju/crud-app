c1dd9a1b08924d4f076a3f05db1395560a26f07d
Raghu K <rgh.199@gmail.com>
2017-01-01 23:18:57 +0530

    Step 2.1: Create store, rootReducer and games reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import rootReducer from './reducers/root.js';
 import './index.css';
+
+const store = createStore(
+	rootReducer,
+);
 
 ReactDOM.render(
   <App />,
