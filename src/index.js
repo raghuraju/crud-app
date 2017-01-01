a308178a77bc55a67966d6576052518be92416c8
Raghu K <rgh.199@gmail.com>
2017-01-01 23:29:18 +0530

    Step 2.2: Apply middlewares to store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/root.js';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 
 const store = createStore(
 	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(thunk)
+	)
 );
 
 ReactDOM.render(
